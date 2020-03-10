c507deeafe7a41774f9732916cd120991c8c7619
Jez Higgins <jez@jezuk.co.uk>
2020-03-10 09:51:24 +0000

    Started compiling patterns into a list of tests.
    I think it's ok so far.

--- src/clojure/brumjs_find.clj
+++ src/clojure/brumjs_find.clj
@@ -1,4 +1,16 @@
-(defn brumjs_find [line] (println line))
+(def pattern (first *command-line-args*))
+(if (nil? pattern)
+    (do (println "Usage: find pattern") (. System exit -1)))
+
+(defn compile_pattern [patt]
+      (loop [source patt result []]
+          (def result-char (take 1 source))
+          (cond
+           (empty? source) result
+           :else (recur (drop 1 source) (conj result [(fn [cand] (= cand result-char)) 1])))))
+
+(def matchfn (compile_pattern pattern))
 
 (doseq [line (line-seq (java.io.BufferedReader. *in*))]
-  (brumjs_find line))
+    (if (matchfn line)
+        (println line)))
