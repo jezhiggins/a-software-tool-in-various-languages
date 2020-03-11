76286a2dbe5fd8f0a1eb92126d308b1b435408a2
Jez Higgins <jez@jezuk.co.uk>
2020-03-11 11:08:33 +0000

    factored out another let in compile-pattern so I only have one call to recur

--- src/clojure/brumjs_find.clj
+++ src/clojure/brumjs_find.clj
@@ -1,5 +1,5 @@
-(def pattern (first *command-line-args*))
-(if (nil? pattern)
+(def pattern-string (first *command-line-args*))
+(if (nil? pattern-string)
   (do (println "Usage: find pattern") (. System exit -1)))
 
 (defn match-char? [cand, expected] (= cand expected))
@@ -8,16 +8,19 @@
 (defn compile-pattern [patt]
   (loop [source patt result []]
     (let [result-char (first source) rest-source (rest source)]
-      (cond
-       (empty? source) result
-       (and (= \$ result-char) (empty? rest-source))
-          (recur rest-source (conj result [end-of-line? 0]))
-       :else (recur rest-source (conj result [(partial match-char? result-char) 1]))))))
+      (let [test-fn
+        (cond
+         (empty? source) nil
+         (and (= \$ result-char) (empty? rest-source)) [end-of-line? 0]
+         :else [(partial match-char? result-char) 1])]
+        (if (nil? test-fn)
+          result
+          (recur rest-source (conj result test-fn)))))))
 
-(def compiled-pattern (compile-pattern pattern))
+(def pattern (compile-pattern pattern-string))
 
 (defn match-line [line-seg]
-  (loop [tests compiled-pattern line line-seg]
+  (loop [tests pattern line line-seg]
     (let [c (first line) ctest (first tests)]
       (let [test (first ctest) advance (first (rest ctest))]
         (if (test c)
@@ -27,7 +30,7 @@
               (recur (rest tests) (drop advance line))))
           false)))))
 
-(defn matchfn [whole-line]
+(defn matcher [whole-line]
   (loop [line whole-line]
     (if (match-line line)
       true
@@ -37,5 +40,5 @@
           (recur rest-of-line))))))
 
 (doseq [line (line-seq (java.io.BufferedReader. *in*))]
-    (if (matchfn line)
+    (if (matcher line)
         (println line)))
