4f1084c7a9a1dc766ddd1fefb99493f6c6439c07
Jez Higgins <jez@jezuk.co.uk>
2020-03-11 10:53:30 +0000

    made names a bit more clojurey

--- src/clojure/brumjs_find.clj
+++ src/clojure/brumjs_find.clj
@@ -2,38 +2,39 @@
 (if (nil? pattern)
   (do (println "Usage: find pattern") (. System exit -1)))
 
-(defn compile_test [])
+(defn match-char? [cand, expected] (= cand expected))
+(defn end-of-line? [cand] (nil? cand))
 
-(defn compile_pattern [patt]
+(defn compile-pattern [patt]
   (loop [source patt result []]
     (let [result-char (first source) rest-source (rest source)]
       (cond
        (empty? source) result
        (and (= \$ result-char) (empty? rest-source))
-          (recur rest-source (conj result [(fn [cand] (nil? cand)) 0]))
-       :else (recur rest-source (conj result [(fn [cand] (= cand result-char)) 1]))))))
+          (recur rest-source (conj result [end-of-line? 0]))
+       :else (recur rest-source (conj result [(partial match-char? result-char) 1]))))))
 
-(def compiled_pattern (compile_pattern pattern))
+(def compiled-pattern (compile-pattern pattern))
 
-(defn match_line [line_seg]
-  (loop [tests compiled_pattern line line_seg]
+(defn match-line [line-seg]
+  (loop [tests compiled-pattern line line-seg]
     (let [c (first line) ctest (first tests)]
       (let [test (first ctest) advance (first (rest ctest))]
         (if (test c)
-          (let [remaining_tests (rest tests)]
-            (if (empty? remaining_tests)
+          (let [remaining-tests (rest tests)]
+            (if (empty? remaining-tests)
               true
               (recur (rest tests) (drop advance line))))
           false)))))
 
-(defn matchfn [whole_line]
-  (loop [line whole_line]
-    (if (match_line line)
+(defn matchfn [whole-line]
+  (loop [line whole-line]
+    (if (match-line line)
       true
-      (let [rest_of_line (rest line)]
-        (if (empty? rest_of_line)
+      (let [rest-of-line (rest line)]
+        (if (empty? rest-of-line)
           false
-          (recur rest_of_line))))))
+          (recur rest-of-line))))))
 
 (doseq [line (line-seq (java.io.BufferedReader. *in*))]
     (if (matchfn line)
